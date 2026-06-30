7c23afc13a5440d7cf0226889b39b9289ae99bdc
Your Name <you@example.com>
2026-06-29 23:12:39 -0500

    add aspect image

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Item = ({ product }) => {
   return (
-    <div className='group relative'>
+    <div className='group relative transition-all duration-500 hover:scale-110 hover:z-2'>
       <Link to={`/products/${product?.id}`}>
-        <div className='aspect-h-1 aspect-w-1 w-full  rounded-md bg-gray-800/50 lg:aspect-none group-hover:opacity-75 lg:h-80 h-96 border border-gray-700/50'>
+        <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-800/50 lg:aspect-none lg:h-80 h-96 border border-gray-700/50'>
           <img
             src={product?.image}
             alt={product?.name}
-            className='h-full w-full object-cover object-center lg:h-full lg:w-full transition-transform duration-700 ease-out group-hover:scale-125'
+            className='h-full w-full object-cover object-center transition-transform duration-700 ease-out group-hover:scale-140'
           />
         </div>
       </Link>
@@ -18,7 +18,7 @@ const Item = ({ product }) => {
         <div>
           <h3 className='text-sm text-foreground'>
             <Link to={`/products/${product?.id}`}>
-              <span aria-hidden="true" className='inset-0'>
+              <span aria-hidden='true'>
                 {product?.name}
               </span>
             </Link>
